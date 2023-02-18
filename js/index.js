c4f822f6c7d42af8145e8746706639c3f12074a9
Arzena Akter <arzenaakter@gmail.com>
2023-02-18 22:49:17 +0600

    completed validation

--- js/index.js
+++ js/index.js
@@ -3,6 +3,18 @@ document.getElementById('blog-btn').addEventListener('click', function(){
     window.location.href = 'blog.html' ;
 });
 
+
+
+
+// card hover background
+document.getElementById('cardbg').addEventListener('mouseenter',function(){
+
+    let number = Math.floor(Math.random()*16777215);
+    let colorCode = '#'+number.toString(16);
+    this.style.backgroundColor = colorCode;
+    console.log(colorCode);
+})
+
 let count = 0;
 // calculation button
 
@@ -14,10 +26,21 @@ document.getElementById('triangle-btn').addEventListener('click',function(){
     const triangleName = document.getElementById('traiangle').innerText;
 
     const triangleinput_1 = document.getElementById('triangle-input-1').value;
+
+    if(triangleinput_1<0){
+        alert('please give a positive number');
+
+        return triangleinput_1;
+    }
     const triangleinput_2= document.getElementById('triangle-input-2').value;
 
+    if(triangleinput_1<0){
+        alert('please give a positive number');
+        triangleinput_2
+    }
+
     const area = 0.5 * parseFloat(triangleinput_1) * parseFloat(triangleinput_2) ;
-    if(isNaN(area)){
+    if(isNaN(area) ){
         alert('Inputs are not valid');
         return area;
     }
@@ -35,7 +58,18 @@ document.getElementById('rectangle-btn').addEventListener('click', function(){
     const rectangleName = document.getElementById('rectangle').innerText;
 
     const rectangleInput_1 = document.getElementById('rectangle-input-1').value;
+
+    if(rectangleInput_1<0){
+        alert('please give a positive number');
+        return rectangleInput_1;
+
+    }
     const rectangleInput_2= document.getElementById('rectangle-input-2').value;
+
+    if(rectangleInput_2<0){
+        alert('please give a positive number');
+        return rectangleInput_2;
+    }
 
     const areaOfrectangle = parseFloat(rectangleInput_1) * parseFloat(rectangleInput_2) ;
 
@@ -57,7 +91,18 @@ document.getElementById('parallelogram-btn').addEventListener('click', function(
     const parallelogramName = document.getElementById('parallelogram').innerText;
 
     const parallelogramInput_1 = document.getElementById('parallelogramInput_1').value;
+    if(parallelogramInput_1<0){
+        alert('please give a positive number');
+        return parallelogramInput_1;
+
+    }
     const parallelogramInput_2= document.getElementById('parallelogramInput_2').value;
+    if(parallelogramInput_2<0){
+        alert('please give a positive number');
+        return parallelogramInput_2;
+
+    }
+    
 
     const areaOfparallelogram = parseFloat(parallelogramInput_1) * parseFloat(parallelogramInput_1) ;
 
@@ -79,7 +124,17 @@ document.getElementById('rhombus-btn').addEventListener('click', function(){
     const rhombusamName = document.getElementById('rhombus').innerText;
 
     const rhombusInput_1 = document.getElementById('rhombusInput_1').value;
+    if( rhombusInput_1<0){
+        alert('please give a positive number');
+        return  rhombusInput_1;
+
+    }
     const rhombusInput_2= document.getElementById('rhombusInput_2').value;
+    if( rhombusInput_2<0){
+        alert('please give a positive number');
+        return  rhombusInput_2;
+
+    }
 
     const areaOfrhombus = 0.5 * parseFloat(rhombusInput_1) * parseFloat(rhombusInput_2) ;
 
@@ -101,7 +156,17 @@ document.getElementById('pentagon-btn').addEventListener('click', function(){
     const rhombusamName = document.getElementById('pentagon').innerText;
 
     const PentagonInput_1 = document.getElementById('PentagonInput_1').value;
+    if( PentagonInput_1<0){
+        alert('please give a positive number');
+        return  PentagonInput_1;
+
+    }
     const PentagonInput_2= document.getElementById('PentagonInput_2').value;
+    if( PentagonInput_2<0){
+        alert('please give a positive number');
+        return  PentagonInput_2;
+
+    }
 
     const areaOfPentagon = 0.5 * parseFloat(PentagonInput_1) * parseFloat(PentagonInput_2) ;
 
@@ -123,7 +188,17 @@ document.getElementById('ellipse-btn').addEventListener('click', function(){
     const EllipseName = document.getElementById('ellipse').innerText;
 
     const ellipseInput_1 = document.getElementById('ellipseInput_1').value;
+    if( ellipseInput_1<0){
+        alert('please give a positive number');
+        return  ellipseInput_1;
+
+    }
     const ellipseInput_2= document.getElementById('ellipseInput_2').value;
+    if( ellipseInput_2<0){
+        alert('please give a positive number');
+        return  ellipseInput_2;
+
+    }
     const pi = 3.14;
 
     const areaOfEllipse = (pi * parseFloat(ellipseInput_1) * parseFloat(ellipseInput_2)).toFixed(2) ;
